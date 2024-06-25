1ba35123488b58af7d96379dee20af3235252316
Mjishu <128941980+Mjishu@users.noreply.github.com.>
2024-06-25 09:15:28 -0400

    started work on del button

--- client/src/components/messageComponents/Messages.jsx
+++ client/src/components/messageComponents/Messages.jsx
@@ -9,7 +9,10 @@ function Messages(){
     const [currentUser, setCurrentUser] = React.useState()
     const [loading, setLoading] = React.useState(true)
     const [allUsers, setAllUsers] = React.useState()
-    const [showUsers,setShowUsers] = React.useState(false)
+    const [showItems,setShowItems] = React.useState({
+        showUsers: false,
+        deleteBox: false,
+    })
     const [allMessages,setAllMessages] = React.useState()
     const navigate = useNavigate()
 
@@ -39,6 +42,7 @@ function Messages(){
             .finally(() => setLoading(false))
     },[currentUser,loading]) //need this to depend on something so i dont have to refresh the page to get it to show new messages
 
+    //Handle Clicking functions//    
 
     function handleClick(id){
         navigate(`/message/${id}`)
@@ -66,23 +70,33 @@ function Messages(){
             fetch("/api/messages/create", {method:'POST', headers:{"Content-Type":"application/json"}, body:JSON.stringify({id:id})})
             .then(res => res.json())
             .then(data => data.message === "Success" && navigate(`/message/${data.id}`))
-            .catch(error => console.error(`error creating message ${error}`))
+            //.catch(error => console.error(`error creating message ${error}`))
         }
     }
 
+    function handleBackdropClick(e){
+        if(e.target.closest('.modalDialog')) return;
+        setShowItems(prevItems=>({...prevItems, showUsers:false}));
+    }
+
+    function deleteMessage(id){ //Make a custom dialog box that says are you sure you want to delete?
+        console.log(id) 
+    }
+
+    // Mapping functions //    
+
     const messagesMapped =  allUserMessages?.map(message => {
         return (
-            <div key={message._id} onClick={() => handleClick(message._id)} className={styles.messagesHolder}>
-            <h3>Author: {message.author.username}</h3>
-            <h4>Recipient: {message.recipient.username} </h4>
+            <div key={message._id}>
+                <div  onClick={() => handleClick(message._id)} className={styles.messagesHolder}>
+                <h3>Author: {message.author.username}</h3>
+                <h4>Recipient: {message.recipient.username} </h4>
+                </div>
+                <button onClick={() => deleteMessage(message._id)}>Del</button>
             </div>
         )
     })
 
-    function handleBackdropClick(e){
-        if(e.target.closest('.modalDialog')) return;
-        setShowUsers(false);
-    }
 
     const mapAllUsers = allUsers?.map(user => { //Doesnt load on conditional
         return(
@@ -103,13 +117,19 @@ function Messages(){
         <h1>Messages</h1>
         {messagesMapped}
         </div> 
-        <button className={styles.searchButton} onClick={() => setShowUsers(true)}><img src="/icons/search.svg" alt="search icon" className={styles.searchButtonImg}/></button>
-        {showUsers && (
+        <button className={styles.searchButton} onClick={() => setShowItems(prevItems => ({...prevItems,showUsers:true }))}><img src="/icons/search.svg" alt="search icon" className={styles.searchButtonImg}/></button>
+        {showItems.showUsers && (
             <div className={styles.dialogBackdrop} onClick={handleBackdropClick}>
                 <div className={styles.customDialog}>
                     {mapAllUsers}
-                    <button onClick={() => setShowUsers(false)}>Close</button>
+                    <button onClick={() => setShowItems(prevItems=>({...prevItems,showUsers:false}))}>Close</button>
                 </div>
+            </div>)}
+        {showItems.deleteBox && ( //idk what to do with this my brain is tired
+            <div>
+                <h3> Are you sure you want to delete this </h3>
+                <button onClick={() => setShowItems(prevItems=>({...prevItems,deleteBox:false}))}>Cancel</button>
+                <button onClick={() => deleteMessage("meow")}>Delete</button>
             </div>
         )}
         </div>
