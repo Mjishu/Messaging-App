30191f90813bb26487e629e64a3f4f0740f3dbf6
Mjishu <128941980+Mjishu@users.noreply.github.com.>
2024-06-28 14:25:21 -0400

    port variable

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ const User = require("./models/User.js")
 const bcrypt = require("bcrypt")
 
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3254
 
 //* Mongoose Connection
 
