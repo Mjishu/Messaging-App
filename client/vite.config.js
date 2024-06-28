6693094c0ecf4630fb8f9015bac598032ce01b8b
Mjishu <128941980+Mjishu@users.noreply.github.com.>
2024-06-28 16:41:41 -0400

    updated viteconfig

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:"http://localhost:3000",
+        target:"https://messaging-app-backend-miwr.onrender.com",
         changeOrigin:true,
       }
     }
